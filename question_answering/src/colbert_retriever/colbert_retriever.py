9e6383d490e2922e995715cafde3929b578d756e
Cristian Berrio <cberrio@expert.ai>
2022-03-08 12:15:44 +0100

    Update colbert_retriever.py

--- question_answering/src/colbert_retriever/colbert_retriever.py
+++ question_answering/src/colbert_retriever/colbert_retriever.py
@@ -9,10 +9,9 @@ class Colbert(object):
     def __init__(self):
         pyterrier_colbert_factory = pyterrier_colbert.ranking.ColBERTFactory(checkpoint, "/home/test/ESA-TDE/colbert", "colbertindex",gpu=False)
         #pyterrier_colbert_factory = pyterrier_colbert.ranking.ColBERTFactory(checkpoint, "/home/test/ESA-TDE/colbert", "colbertindex",gpu=True)
-        pyterrier_colbert_factory_qa = pyterrier_colbert.ranking.ColBERTFactory(checkpoint, "/home/test/ESA-TDE/colbert", "qa-colbertindex",gpu=False)
-        #pyterrier_colbert_factory_qa = pyterrier_colbert.ranking.ColBERTFactory(checkpoint, "/home/test/ESA-TDE/colbert", "qa-colbertindex",gpu=True)
-        self.colbert_e2e = {"CDF":pyterrier_colbert_factory.end_to_end(),"QA":pyterrier_colbert_factory_qa.end_to_end()}
 
-    def get_contexts(self, question, index = "CDF"):
-        results = (self.colbert_e2e[index] % 10).search(question)
+        self.colbert_e2e = pyterrier_colbert_factory.end_to_end()
+
+    def get_contexts(self, question):
+        results = (self.colbert_e2e % 10).search(question)
         return [int(doc_id)-1 for doc_id,score in zip(results['docno'].tolist(),results['score'].tolist()) if score>=19]
